87242ad7f78258f246272dad7dfb8e83ae5381bd
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-10-15 16:32:06 +0300

    Added shellscript to run jUnit 5 standalone console tests.

---
+++ runtests.sh
@@ -0,0 +1,2 @@
+java -jar ${HOME}/.m2/repository/org/junit/platform/junit-platform-console-standalone/1.3.1/junit-platform-console-standalone-1.3.1.jar --class-path ./target/classes:./target/test-classes --scan-class-path
+# --include-package handlebarstest # --select-directory target/test-classes/handlebarstest/ 
